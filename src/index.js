292c589377c266e729985e3835e0fde6455ee2a8
thais.calixto <thais.calixto@mail.uft.edu.br>
2022-04-15 18:53:01 -0300

    connect to database
    import mongoose and dotenv to help database connection

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
 import patientRouter from "./router/PatientRouter.js";
 
-const PORT = 3000;
+dotenv.config();
+
+const DATABASE_URL = process.env.DATABASE_URL;
+const PORT = process.env.PORT;
+
+mongoose
+	.connect(DATABASE_URL)
+	.then(() => {
+		console.log("Database connected");
+	})
+	.catch((error) => {
+		console.error("Error to connect to database: " + error.message);
+	});
 
 const app = express();
 
