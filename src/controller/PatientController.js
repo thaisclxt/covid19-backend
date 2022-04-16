0d5c910fc071570c7899409f3d908764b1ed5654
thais.calixto <thais.calixto@mail.uft.edu.br>
2022-04-16 17:31:53 -0300

    find all schedule on date
    if `scheduleDateStart` is greater than or equal to `scheduleDate`'s
    patient and `scheduleDateEnd` is less than `scheduleDate`'s patient

--- src/controller/PatientController.js
+++ src/controller/PatientController.js
@@ -10,13 +10,22 @@ class PatientController {
 		try {
 			const { name, birthDate, scheduleDate } = request.body;
 
+			const scheduleDateStart = new Date(scheduleDate.substring(0, 10));
+			const scheduleDateEnd = new Date(
+				scheduleDateStart.getTime() + 24 * 60 * 60 * 1000
+			);
+
+			const scheduleOnDate = await patientModel.find({
+				scheduleDate: { $gte: scheduleDateStart, $lt: scheduleDateEnd },
+			});
+
 			const patient = await patientModel.create({
 				name,
 				birthDate,
 				scheduleDate,
 			});
 
-			response.send({ message: "Patient created", patient: patient });
+			response.send({ message: "Patient created", patient, scheduleOnDate });
 		} catch (error) {
 			response.status(500).send({ message: "Something went wrong" });
 			console.log(error);
